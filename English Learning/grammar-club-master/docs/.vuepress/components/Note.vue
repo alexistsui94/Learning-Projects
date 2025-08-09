eec15580c5bbe4b8d6a4ce3672f56f2c580bc0f0
alexistsui94 <alexistsui94@gmail.com>
2025-08-09 11:11:26 +0800

    Add grammar-club content under English Learning

---
+++ English Learning/grammar-club-master/docs/.vuepress/components/Note.vue
@@ -0,0 +1,33 @@
+<template>
+  <span class="note_main">
+    <span v-if="normal"><slot /></span>
+    <u v-else-if="ul"><slot /></u>
+    <b v-else class="note_title"><slot /></b>
+
+    <span v-if="note">
+      <span v-if="ul">{{ note }}</span>
+      <b v-else>{{ note }}</b>
+    </span>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    normal: Boolean, // 无下划线，note 加粗
+    ul: Boolean, // 有下划线，note 正常
+    note: String
+  }
+};
+</script>
+
+<style lang="stylus">
+.note_main
+  display: inline-grid;
+  text-align: center;
+  text-indent: 0;
+  line-height: 1.2;
+
+.note_title
+  text-decoration: underline;
+</style>
---
+++ English Learning/grammar-club-master/docs/.vuepress/config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  plugins: [
+    [
+      'google-analytics-4',
+      {
+        gtag: 'G-423T4HSGBR'
+      }
+    ]
+  ],
+  base: process.env.GITHUB == 'github' ? '/grammar-club/' : '/',
+  dest: process.env.GITHUB == 'github' ? 'docs/.vuepress/github' : 'docs/.vuepress/dist',
+  title: '语法俱乐部',
+  themeConfig: {
+    repo: 'llwslc/grammar-club',
+    displayAllHeaders: true,
+    smoothScroll: true,
+    sidebar: {
+      '/content/': [
+        ['Preface', '序'],
+        'Introduction',
+        'Contents',
+        {
+          title: '第一篇 初级句型--简单句',
+          collapsable: false,
+          children: [
+            'Chapter01',
+            'Chapter02',
+            'Chapter03',
+            'Chapter04',
+            'Chapter05',
+            'Chapter06',
+            'Chapter07',
+            'Chapter08',
+            'Chapter09',
+            'Chapter10',
+            'Chapter11'
+          ]
+        },
+        {
+          title: '第二篇 中级句型--复句',
+          collapsable: false,
+          children: ['Chapter12', 'Chapter13', 'Chapter14', 'Chapter15']
+        },
+        {
+          title: '第三篇 高级句型--简化从句',
+          collapsable: false,
+          children: ['Chapter16', 'Chapter17', 'Chapter18', 'Chapter19', 'Chapter20', 'Chapter21', 'Chapter22']
+        }
+      ]
+    },
+    nav: [{ text: '目录', link: '/content/Preface' }]
+  }
+};
